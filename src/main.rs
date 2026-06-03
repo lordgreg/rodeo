e5b66a51c35ed817ab2c9fdb645064c8e4ece3d9
grepx <grepx@noreply.codeberg.org>
2026-06-04 00:16:09 +0200

    Update main return type

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod cli;
 mod config;
 mod ui;
 
-fn main() {
+fn main() -> color_eyre::Result<()> {
     // env_logger::init();
     env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
 
@@ -14,5 +14,5 @@ fn main() {
     let config = config::load_config(args.config.as_deref());
     println!("Config: {:?}", config);
 
-    ui::run();
+    ui::run()
 }
